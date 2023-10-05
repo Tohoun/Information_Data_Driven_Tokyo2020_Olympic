aa8406ef9ba39e31f7f34eccee8ad17c8696ac71
Tohoun <romeotohoun@gmail.com>
2023-10-05 21:28:06 +0400

    Evolution of repot

--- src/dataMartStructure.sql
+++ src/dataMartStructure.sql
@@ -2,7 +2,7 @@
 CREATE DATABASE OLYMPIC2020;
 
 --2. Create Athletes tables
-CREATE TABLE D_ATHLETES (
+CREATE TABLE IF NOT EXISTS D_ATHLETES (
     id SERIAL PRIMARY KEY,
     name VARCHAR(150) NOT NULL,
     gender VARCHAR(50),
@@ -12,7 +12,7 @@ CREATE TABLE D_ATHLETES (
 );
 
 --3. Create table Coaches
-CREATE TABLE D_COACHES (
+CREATE TABLE IF NOT EXISTS D_COACHES (
     id SERIAL PRIMARY KEY,
     name VARCHAR(150) NOT NULL,
     gender VARCHAR(50),
@@ -23,7 +23,7 @@ CREATE TABLE D_COACHES (
 );
 
 --4. Create table Medals details
-CREATE TABLE F_MEDALS_DETAILS (
+CREATE TABLE IF NOT EXISTS F_MEDALS_DETAILS (
     id SERIAL PRIMARY KEY,
     medal_code INT,
     medal_date date,
@@ -34,7 +34,7 @@ CREATE TABLE F_MEDALS_DETAILS (
 );
 
 --5. Create table Countries
-CREATE TABLE R_COUNTRIES (
+CREATE TABLE IF NOT EXISTS R_COUNTRIES (
     id SERIAL PRIMARY KEY,
     name VARCHAR(150),
     country_code VARCHAR(3),
@@ -43,15 +43,15 @@ CREATE TABLE R_COUNTRIES (
 );
 
 --6. create table medals
-CREATE TABLE R_MEDALS (
+CREATE TABLE IF NOT EXISTS R_MEDALS (
     id SERIAL PRIMARY KEY,
     medal_code INT,
-    medal_name VARCHAR(50),
+    medal_name VARCHAR(50)
 );
 
 --7. create table discipline
-CREATE TABLE R_DISCIPLINES (
+CREATE TABLE IF NOT EXISTS R_DISCIPLINES (
     id SERIAL PRIMARY KEY,
     discipline_code VARCHAR(4),
-    discipline_name VARCHAR(50),
+    discipline_name VARCHAR(50)
 );
